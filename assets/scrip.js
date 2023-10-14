0403781f0a2c1b8c0ff6b5e2f6e12c91a93c9a17
Jonatan Ferreira <jonatan91668954@gmail.com>
2023-10-14 02:05:55 -0300

    Primeiro Commit

---
+++ assets/scrip.js
@@ -0,0 +1,41 @@
+function calculadora() {
+    var num1 = parseInt(document.querySelector(".num1").value);
+
+    var sinal = document.querySelector(".sinal").value;
+
+    var num2 = parseInt(document.querySelector(".num2").value);
+
+    var total = 0;
+
+    if(isNaN(num1) && sinal == "" && isNaN(num2)){
+        alert("Campo Vazio");
+    }else if (sinal == "+") {
+        var total = num1 + num2;
+
+        parseInt(document.querySelector(".num1").value = "");
+        parseInt(document.querySelector(".sinal").value = "");
+        parseInt(document.querySelector(".num2").value = "");
+    }else if(sinal == "-") {
+        var total = num1 - num2;
+
+        parseInt(document.querySelector(".num1").value = "");
+        parseInt(document.querySelector(".sinal").value = "");
+        parseInt(document.querySelector(".num2").value = "");
+    }else if(sinal == "/"){
+        var total = num1 / num2;
+
+        parseInt(document.querySelector(".num1").value = "");
+        parseInt(document.querySelector(".sinal").value = "");
+        parseInt(document.querySelector(".num2").value = "");
+    }else if(sinal == "*"){
+        var total = num1 * num2;
+        
+        parseInt(document.querySelector(".num1").value = "");
+        parseInt(document.querySelector(".sinal").value = "");
+        parseInt(document.querySelector(".num2").value = "");
+    }
+
+    console.log(total)
+
+    document.querySelector(".resultado").innerHTML = "Resultado: "+total
+}
